eab17b49b2d512a43e6bc7736f4300afda4835ed
Jorge Antony Zarate Davila <antonijorge987@gmail.com>
2025-08-09 22:39:22 -0500

    update

--- index.js
+++ index.js
@@ -16,7 +16,20 @@ app.use(express.json());
 
 // Helper function to avoid code repetition
 async function getSheetData(sheetName, range) {
-  const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+  const credentials = {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  };
+  
   const auth = new google.auth.GoogleAuth({
     credentials,
     scopes: "https://www.googleapis.com/auth/spreadsheets",
@@ -38,7 +51,20 @@ async function getSheetData(sheetName, range) {
 
 // Helper function to append data to sheet
 async function appendSheetData(sheetName, range, values) {
-  const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+  const credentials = {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  };
+  
   const auth = new google.auth.GoogleAuth({
     credentials,
     scopes: "https://www.googleapis.com/auth/spreadsheets",
