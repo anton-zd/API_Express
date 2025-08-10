aca7074b9d6974d5f9a52e216fb3045e946c4afd
Jorge Antony Zarate Davila <antonijorge987@gmail.com>
2025-08-09 22:51:07 -0500

    update

--- index.js
+++ index.js
@@ -23,17 +23,14 @@ function getCredentials() {
   
   // If running in production (Railway with environment variables)
   return {
-    type: process.env.GOOGLE_TYPE,
+    type: "service_account",
     project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
     private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_AUTH_URI,
-    token_uri: process.env.GOOGLE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    universe_domain: "googleapis.com"
   };
 }
 
